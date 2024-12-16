d0867b88e0e7873a9f44eb2f370e7af795442737
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2024-12-16 16:13:23 +0500

    Revert "remove tooltips due to interference with dialog triggers"
    This reverts commit 74f90dc366086e98d4d72059d143d220f80b563b.

--- src/components/edit-dialog-button.tsx
+++ src/components/edit-dialog-button.tsx
@@ -2,15 +2,30 @@
 
 import DNSDialogForm from "@/components/dns-dialog-form";
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { DNSEntry } from "@/lib/definitions";
 import { Edit } from "lucide-react";
 
 const EditDialogButton = ({ entry }: { entry: DNSEntry }) => {
   return (
     <DNSDialogForm mode="edit" initialData={entry}>
-      <Button variant="ghost" size="icon">
-        <Edit />
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Edit />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Edit</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </DNSDialogForm>
   );
 };
